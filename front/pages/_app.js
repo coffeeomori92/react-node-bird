8fd09e4d23d7b77f50b9741291e23caa51a8e13d
coffeemori92 <coffeemori92@naver.com>
2020-04-13 22:28:56 +0900

    TODO react-redux connect

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Head from 'next/head';
-import AppLayout from '../components/AppLayout';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import withRedux from 'next-redux-wrapper';
 
-const NodeBird = ({ Component }) => {
+import AppLayout from '../components/AppLayout';
+import reducer from '../reducers';
+
+const NodeBird = ({ Component, store }) => {
     return (
-        <>
+        <Provider store={store}>
         <Head>
             <title>NodeBird</title>
             <link 
@@ -15,8 +20,16 @@ const NodeBird = ({ Component }) => {
         <AppLayout>
             <Component />
         </AppLayout>
-        </>
+        </Provider>
     );
 };
 
-export default NodeBird;
+export default withRedux((initialState, options) => {
+    const middlewares = [];
+    const enhancer = compose(
+        applyMiddleware(...middlewares),
+        !options.isServer && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+        );
+    const store = createStore(reducer, initialState, enhancer);
+    return store;
+})(NodeBird);
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import PostForm from '../components/PostForm';
 import PostCard from '../components/PostCard';
+import { LOG_IN } from '../reducers/user';
 
 const dummy = {
     isLoggedIn : true,
@@ -17,8 +19,20 @@ const dummy = {
 };
 
 const Home = () => {
+    const dispatch = useDispatch();
+    const { isLoggedIn, user } = useSelector(state => state.user);
+    useEffect(() => {
+        dispatch({
+            type: LOG_IN,
+            data: {
+                nickname: '커피모리'
+            }
+        });
+    }, []);
+
     return (
         <>
+            {user ? <div>로그인 했습니다: {user.nickname}</div> : <div>로그아웃 했습니다.</div>}
             {dummy.isLoggedIn && <PostForm />}
             {dummy.mainPosts.map((c) => {
                 return (
